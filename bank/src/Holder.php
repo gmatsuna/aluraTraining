784278349a4bc8fcc38e8a1bae81105f38f4b02a
Gilberto Hideaki Matsunaga <60249940+gmatsuna@users.noreply.github.com>
2024-02-23 15:09:09 -0300

    implemented cpf validation

--- bank/src/Holder.php
+++ bank/src/Holder.php
@@ -8,6 +8,7 @@ class Holder
     public function __construct(string $name, string $cpf)
     {
         $this->name = $name;
+        $this->cpfValidate($cpf);
         $this->cpf = $cpf;
     }
     public function getName(): string
@@ -17,5 +18,13 @@ class Holder
     public function getCpf(): string
     {
         return $this->cpf;
+    }
+    public function cpfValidate($cpf)
+    {
+        $regex = '/^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$/';
+        if (!preg_match($regex, $cpf)) {
+            throw new \Exception('CPF inválido');
+        }
+        var_dump($cpf);
     }
 }
