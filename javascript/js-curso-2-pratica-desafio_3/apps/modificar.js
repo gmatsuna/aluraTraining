00c001aca8457e3379a442e8f508164746b3b3a2
Gilberto Hideaki Matsunaga <gmatsuna@gmail.com>
2024-08-14 15:33:46 -0300

    file and directory organization_new exercises

--- javascript/js-curso-2-pratica-desafio_3/apps/modificar.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("h1").innerHTML = "Modificar conteudo."
-document.getElementById("descricao").innerHTML = `
-    Tenha um elemento HTML na página (por exemplo, um parágrafo) e utilize JavaScript para modificar seu conteúdo usando a propriedade textContent.
-`;
-
-document.getElementById("p1").textContent = "Texto Modificado. Aqui ja foi feito a modificacao.";
