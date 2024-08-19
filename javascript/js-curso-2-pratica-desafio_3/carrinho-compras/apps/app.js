c0904d13e34328a9f5dc3461741afc3ccec71e98
Gilberto Hideaki Matsunaga <gmatsuna@gmail.com>
2024-08-19 20:25:19 -0300

    exercicio carrinho de compras

--- javascript/js-curso-2-pratica-desafio_3/carrinho-compras/apps/app.js
+++ javascript/js-curso-2-pratica-desafio_3/carrinho-compras/apps/app.js
@@ -1,5 +1,29 @@
+let total = 0;
+
 function adicionar() {
     var produto = document.getElementById('produto').value;
+    var qtd = document.getElementById('qtd').value;
 
-    console.log('produto');
+    var productName = produto.split(' - ')[0];
+    var productValue = produto.split('$')[1];
+
+    var itemSum = qtd*productValue;
+
+    var itemList = document.getElementById('container-list-products');
+    itemList.innerHTML = itemList.innerHTML + `
+        <section class="carrinho__produtos__produto">
+            <span class="texto-azul">${qtd}x</span> ${productName} <span class="texto-azul">R$${itemSum},00</span>
+        </section>
+    `;
+
+
+    total += itemSum;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `Total: R$${total},00`;
+
+
+
+    
+
+    console.log(itemSum);
 }
